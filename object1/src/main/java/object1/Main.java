ce8ed27b66bf1b5e178f442fd5e4506d2ef17992
Robbie-Niemeyer3 <robbie.niemeyer3@gmail.com>
2024-06-26 19:40:41 -0400

    Practice Object1

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -1,10 +1,30 @@
-package object1;
+//package object1;
 
 import java.util.logging.Logger;
 
 public class Main {
 
+    // "Units are centimeters" //
+
     public static void main(String... args) {
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info("This i s a module-using Hello World!");
+        Person dan = new Person( "Robbie", 32);
+        dan.setAge(88);
+        dan.getAge();
+        System.out.println(dan.getAge());
+
+        Person mo = new Person("Mo", 25);
+        mo.setAge(21);
+        mo.getAge();
+        System.out.println(mo.getAge());
+
+    }
+
+
+
+
+
+
+
 }
---
+++ object1/src/main/java/object1/Person.java
@@ -0,0 +1,41 @@
+// Define a class
+class Person {
+    // Define Instance Variables
+    // different for each object!
+
+    private String name;
+    private int age;
+    private int height;
+    // centimeters
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
+
